5d95c0dfd6b6bd5fc46b748b821c6ba9e367fadb
vaidehi2401 <dholevaidehi2@gmail.com>
2025-02-05 12:37:42 +0530

    Added html and css

--- routes/contact.js
+++ routes/contact.js
@@ -2,9 +2,8 @@ const express= require('express');
 const path = require('path');
 const rootDir = require('../utils/path')
 const router = express.Router();
-router.use('/add-product',(req, res, next)=>{
-    res.sendFile(path.join(rootDir , 'views', 'admin.html'))
-  
+router.use('/',(req, res, next)=>{
+    res.sendFile(path.join(rootDir , 'views', 'contact.html'))
 })
 router.post('/',(req, res, next)=>{
     console.log(req.body);
